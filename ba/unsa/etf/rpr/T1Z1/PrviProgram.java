f06e092e232428c6d3d7b952789499bab7c8ce41
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2020-10-22 13:18:46 +0200

    pocetna struktura

---
+++ ba/unsa/etf/rpr/T1Z1/PrviProgram.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.T1Z1
+
+import java.util.Scanner;
+
+public class PrviProgram {
+	public static void main(String[] Args) {
+		Scanner ulaz = new Scanner(System.in);
+		
+		
+	}
+}

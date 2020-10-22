26874f281e04afa9472ad4fd88007c7728a98b80
evlahovlja1 <evlahovlja1@etf.unsa.ba>
2020-10-22 13:37:31 +0200

    zavrseno

---
+++ README.md
@@ -0,0 +1,1 @@
+C:\Users\elvir\Desktop\etf 2020_2021\RPR\TUT1\Z1>java -cp . ba.unsa.etf.rpr.T1Z1.PrviProgram
--- ba/unsa/etf/rpr/T1Z1/PrviProgram.java
+++ ba/unsa/etf/rpr/T1Z1/PrviProgram.java
@@ -1,4 +1,4 @@
-package ba.unsa.etf.rpr.T1Z1
+package ba.unsa.etf.rpr.T1Z1;
 
 import java.util.Scanner;
 
@@ -6,6 +6,12 @@ public class PrviProgram {
 	public static void main(String[] Args) {
 		Scanner ulaz = new Scanner(System.in);
 		
+		int a, b;
 		
+		System.out.println("Unesite dva broja: ");
+		a = ulaz.nextInt();
+		b = ulaz.nextInt();
+		
+		System.out.println("Prvi broj: " + a + ", a drugi broj: " + b);
 	}
 }
